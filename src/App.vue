746e90d33438135e80f272e15b102e1930fc3844
nelsonluz <nelsondsluz@gmail.com>
2018-07-02 14:06:32 -0300

    Arquivos da pasta

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div> 
+     <h1>{{ titulo }}</h1>
+     <img :src="foto.url" :alt="foto.titulo">
+  </div>
+</template>
+
+<script>
+export default {
+
+  data(){ 
+    return {
+      titulo: 'Alurapic',
+      foto: {
+        url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It',
+        titulo: 'Cachorro'
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
